a2105250eee48071165b0f940361502ad9a6e447
aziz-belloumi <belloumiaziz1@gmail.com>
2024-09-13 23:30:21 +0100

    module project web s1

---
+++ bulck.js
@@ -0,0 +1,8 @@
+function toggleHiddenInfo() {
+    var hiddenInfo = document.getElementById("hiddenInfo");
+    if (hiddenInfo.style.display === "none") {
+        hiddenInfo.style.display = "block";
+    } else {
+        hiddenInfo.style.display = "none";
+    }
+}
---
+++ exercice.js
@@ -0,0 +1,32 @@
+function toggleHiddenInfo1() {
+    var hiddenInfo = document.getElementById("hiddentips1");
+    if (hiddenInfo.style.display === "none") {
+        hiddenInfo.style.display = "block";
+    } else {
+        hiddenInfo.style.display = "none";
+    }
+}
+function toggleHiddenInfo2() {
+    var hiddenInfo = document.getElementById("hiddentips2");
+    if (hiddenInfo.style.display === "none") {
+        hiddenInfo.style.display = "block";
+    } else {
+        hiddenInfo.style.display = "none";
+    }
+}
+function toggleHiddenInfo3() {
+    var hiddenInfo = document.getElementById("hiddentips3");
+    if (hiddenInfo.style.display === "none") {
+        hiddenInfo.style.display = "block";
+    } else {
+        hiddenInfo.style.display = "none";
+    }
+}
+function toggleHiddenInfo4() {
+    var hiddenInfo = document.getElementById("hiddentips4");
+    if (hiddenInfo.style.display === "none") {
+        hiddenInfo.style.display = "block";
+    } else {
+        hiddenInfo.style.display = "none";
+    }
+}
